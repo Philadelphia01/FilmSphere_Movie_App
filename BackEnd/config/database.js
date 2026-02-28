449e0efcb39395ea96b8ed6cdd3ad01ea3f1908f
Philadelphia01 <nkunalotricia@gmail.com>
2026-03-01 00:11:11 +0200

    fixed database onnection

--- BackEnd/config/database.js
+++ BackEnd/config/database.js
@@ -3,13 +3,12 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const pool = new Pool({
-  host: process.env.DB_HOST || "localhost",
-  port: process.env.DB_PORT || 5432,
-  database: process.env.DB_NAME || "filmsphere",
-  user: process.env.DB_USER || "postgres",
-  password: String(process.env.DB_PASSWORD),
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
 });
-
 const databaseConnection = async () => {
   try {
     const client = await pool.connect();
